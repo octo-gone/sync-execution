f1e45f3d23ef1f4bdf96281b4eb5a4901ef2a106
octo-gone <nklimin007@gmail.com>
2020-07-30 23:31:13 +0300

    [upd] num class power updated

--- scripts/utils/utils.py
+++ scripts/utils/utils.py
@@ -47,19 +47,19 @@ def coercion(value):
 class Number(Real):
 
     def __round__(self, ndigits=None):
-        return Number(self.value.__round__(ndigits))
+        return self.__class__(self.value.__round__(ndigits))
 
     def __floordiv__(self, other):
-        return Number(self.value.__floordiv__(other))
+        return self.__class__(self.value.__floordiv__(other))
 
     def __rfloordiv__(self, other):
-        return Number(self.value.__rfloordiv__(other))
+        return self.__class__(self.value.__rfloordiv__(other))
 
     def __mod__(self, other):
-        return Number(self.value.__mod__(other))
+        return self.__class__(self.value.__mod__(other))
 
     def __rmod__(self, other):
-        return Number(self.value.__rmod__(other))
+        return self.__class__(self.value.__rmod__(other))
 
     def __lt__(self, other) -> bool:
         if type(other) is Number:
@@ -72,37 +72,40 @@ class Number(Real):
         return self.value.__le__(other)
 
     def __radd__(self, other):
-        return Number(self.value.__radd__(other))
+        return self.__class__(self.value.__radd__(other))
 
     def __neg__(self):
-        return Number(self.value.__neg__())
+        return self.__class__(self.value.__neg__())
 
     def __pos__(self):
-        return Number(self.value.__pos__())
+        return self.__class__(self.value.__pos__())
 
     def __mul__(self, other):
-        return Number(self.value.__mul__(other))
+        return self.__class__(self.value.__mul__(other))
 
     def __rmul__(self, other):
-        return Number(self.value.__rmul__(other))
+        return self.__class__(self.value.__rmul__(other))
 
     def __div__(self, other):
-        return Number(self.value.__div__(other))
+        return self.__class__(self.value.__div__(other))
 
     def __rdiv__(self, other):
-        return Number(self.value.__rdiv__(other))
+        return self.__class__(self.value.__rdiv__(other))
 
     def __truediv__(self, other):
-        return Number(self.value.__truediv__(other))
+        return self.__class__(self.value.__truediv__(other))
 
     def __rtruediv__(self, other):
-        return Number(self.value.__rtruediv__(other))
+        return self.__class__(self.value.__rtruediv__(other))
 
-    def __pow__(self, exponent):
-        return Number(self.value.__pow__(exponent))
+    def __pow__(self, exponent, modulo=None):
+        if modulo is None:
+            return self.__class__(self.value ** exponent)
+        else:
+            return self.__class__(pow(self.value, exponent, modulo))
 
     def __rpow__(self, base):
-        return Number(self.value.__rpow__(base))
+        return self.__class__(self.value.__rpow__(base))
 
     def __hash__(self) -> int:
         return hash(self.value)
@@ -120,13 +123,13 @@ class Number(Real):
         return str(self.number_coercion(self.value))
 
     def __abs__(self):
-        return Number(self.value.__abs__())
+        return self.__class__(self.value.__abs__())
 
     def __add__(self, other):
-        return Number(self.value.__add__(other))
+        return self.__class__(self.value.__add__(other))
 
     def __ceil__(self):
-        return Number(self.value.__ceil__())
+        return self.__class__(self.value.__ceil__())
 
     def __eq__(self, other):
         if type(other) is Number:
@@ -134,10 +137,10 @@ class Number(Real):
         return self.value.__eq__(other)
 
     def __floor__(self):
-        return Number(self.value.__floor__())
+        return self.__class__(self.value.__floor__())
 
     def __trunc__(self):
-        return Number(self.value.__trunc__())
+        return self.__class__(self.value.__trunc__())
 
     def __ge__(self, other) -> bool:
         if type(other) is Number:
@@ -150,13 +153,13 @@ class Number(Real):
         return self.value.__gt__(other)
 
     def __iadd__(self, other):
-        return Number(self.value + other)
+        return self.__class__(self.value + other)
 
     def __isub__(self, other):
-        return Number(self.value - other)
+        return self.__class__(self.value - other)
 
     def __imul__(self, other):
-        return Number(self.value * other)
+        return self.__class__(self.value * other)
 
     @staticmethod
     def number_coercion(value):
