1adfda62f2106a11dd83fff5061b371fecff6968
octo-gone <nklimin007@gmail.com>
2020-08-06 18:19:24 +0300

    docs: added documentation for class 'Node'

--- scripts/nodes/base.py
+++ scripts/nodes/base.py
@@ -1,4 +1,4 @@
-from abc import abstractmethod, ABC
+from abc import abstractmethod
 from scripts.utils import nodes_v8 as nodes_info
 from scripts.utils import logger
 
@@ -11,17 +11,34 @@ one_connector = ("int", "real", "obj", "char", "ctrl", "bool", "any", "number", 
 seven_connectors = ("mult", )
 five_connectors = ("dir_mult", )
 
+# basic states
 ACTIVE = "active"
 INACTIVE = "inactive"
 WAITING = "waiting"
 
 
 class Node:
+    """
+    Create a new node object from the given data. Node has
+    some abstract methods, therefore it is better to use
+    this class as parent class for specific node.
+    """
     variables = {}
     struct_variables = {}
     nodes = {}
 
     def __init__(self, data):
+        """
+        Node constructor. Attribute takes dictionary with
+        several descriptive parameters:
+            id - identifier in xml scheme;
+            node_name - name in sync language;
+            value - description entered in scheme;
+            inputs/outputs - input/output connectors position on node (list of floats);
+            x/y/width/height - position and size of node in scheme.
+
+        :param dict data: information about node
+        """
         self.id = data["id"]
         self.nodes[self.id] = self
         self.raw_data = data
@@ -29,6 +46,7 @@ class Node:
         self.name = data["node_name"]
         self.desc_value = data["value"]
 
+        print(data["inputs"], data["outputs"])
         self.inputs = [[] for _ in range(len(data["inputs"]))]
         self.outputs = [[] for _ in range(len(data["outputs"]))]
 
@@ -49,28 +67,55 @@ class Node:
         self.size = float(data["width"]), float(data["height"])
 
     def get_actual_input(self, input_index):
+        """
+        Function converts input index from scheme to node
+        input index. This is required, since multiple inputs
+        on the diagram can come to a single node input.
+
+        :param int input_index: index in scheme
+        :return int: index in node
+        """
         for key, value in self.actual_inputs.items():
             if input_index in value:
                 return key
 
     def get_actual_output(self, output_index):
+        """
+        Function converts output index from scheme to node
+        output index. This is required, since multiple outputs
+        on the diagram can come to a single node output.
+
+        :param int output_index: index in scheme
+        :return int: index in node
+        """
         for key, value in self.actual_outputs.items():
             if output_index in value:
                 return key
 
     def get_actual_state(self):
+        """
+        Function generates string with state from base
+        state and additional state.
+
+        :return str: <base_state>_<sub_state>
+        """
         sub_state = "_" + self.sub_state if self.sub_state is not None else ""
         return f"{self.state}{sub_state}"
 
     def __del__(self):
+        """
+        Removes node from the dictionary of all nodes.
+        """
         if self.id in self.nodes:
             self.nodes.pop(self.id)
 
-    def reset(self, *args, **kwargs):
-        self.__class__.__init__(self, self.raw_data, *args, **kwargs)
-        self.update_connections()
-
     def update_connections(self):
+        """
+        Function generates information about the connections
+        of node inputs and inputs on the diagram. Checks whether
+        the connection is correct. Creates relations between
+        nodes, this requires working with the class Wire.
+        """
         for i in range(len(self.inputs)):
             wires = Wire.get_nodes_from_input(self, i)
             self.inputs[i] = wires
@@ -141,6 +186,16 @@ class Node:
             self.output_values.append(None)
 
     def get_value(self, input_index, mult=False):
+        """
+        Function prepares and returns values from
+        other nodes connected to this node. If the node inputs
+        can connect more than one wire, the function will
+        combine values from all nodes into a list.
+
+        :param int input_index: actual input of node
+        :param bool mult: consider the input as multiple input or not
+        :return: value from input
+        """
         if mult:
             values = []
             for input_node, output_index in self.inputs[input_index]:
@@ -150,13 +205,30 @@ class Node:
             return input_node.output_values[output_index]
 
     def set_active(self, output_index):
+        """
+        Function runs set_state for all nodes connected to the specified output.
+
+        :param int output_index: actual output of node
+        """
         for output_node, input_index in self.outputs[output_index]:
             output_node.set_state(WAITING, input_index, obj=self, output_index=output_index)
 
     def set_state(self, state, input_index, **kwargs):
+        """
+        Changes state. Can be redefined.
+
+        :param str state: new state
+        :param int input_index: index of input from which the state change is requested
+        :param kwargs: additional arguments if needed (obj and output_index)
+        """
         self.state = state
 
     def update(self, state):
+        """
+        Function selects the update type depending on the current state.
+
+        :param str state: update state
+        """
         if len({self.state, state, WAITING}) == 1:
             self.update_waiting()
         if len({self.state, state, ACTIVE}) == 1:
@@ -166,13 +238,22 @@ class Node:
 
     @abstractmethod
     def update_active(self):
+        """
+        Abstract function. It runs if state is ACTIVE
+        """
         pass
 
     @abstractmethod
     def update_waiting(self):
+        """
+        Abstract function. It runs if state is WAITING
+        """
         pass
 
     def update_inactive(self):
+        """
+        Abstract function. It runs if state is INACTIVE
+        """
         pass
 
 
