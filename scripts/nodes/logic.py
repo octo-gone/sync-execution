3e8b003218d58486d690d8390d44fe8ca409407c
octo-gone <nklimin007@gmail.com>
2020-08-14 14:39:41 +0300

    docs: added documentation for nodes in group 'logic'

--- scripts/nodes/logic.py
+++ scripts/nodes/logic.py
@@ -4,15 +4,40 @@ from scripts.nodes.base import ACTIVE, WAITING, INACTIVE
 
 
 class NodeLogicA(base.Node):
+    """
+    Class for nodes 'and', 'or', 'not and', 'not or', 'equal'.
+    Operators with multiple input.
+    """
+
     def __init__(self, data):
+        """
+        Class constructor. Creates variables:
+            variant - mode of node:
+                all - node waits all signals from inputs;
+                any - node waits first signal from inputs.
+
+        :param dict data: node information
+        """
         super().__init__(data)
         self.variant = all if self.desc_value != "any" else any
 
     def update_active(self):
+        """
+        Update function, runs if state is ACTIVE.
+
+        Resets node and activates next nodes.
+        """
         self.set_active(0)
         self.state = INACTIVE
 
     def update_waiting(self):
+        """
+        Update function, runs if state is WAITING.
+
+        If all items (or any - at least one item) from input are not None
+        then function evaluates operation. Before evaluation function clears
+        list of values from None.
+        """
         if self.variant(map(lambda x: x is not None, self.get_value(0, True))):
             values = list(filter(lambda x: x is not None, self.get_value(0, True)))
             if self.name == "and":
@@ -21,7 +46,7 @@ class NodeLogicA(base.Node):
                 self.output_values[0] = any(values)
             elif self.name == "not and":
                 self.output_values[0] = not all(values)
-            elif self.name == "not or":
+            elif self.name == "nor or":
                 self.output_values[0] = not any(values)
             elif self.name == "equal":
                 self.output_values[0] = len(set(values)) == 1
@@ -29,12 +54,26 @@ class NodeLogicA(base.Node):
 
 
 class NodeLogicB(base.Node):
+    """
+    Class for nodes 'greater', 'greater or equal', 'less', 'less or equal', 'not equal', 'xor'.
+    Operators with two inputs.
+    """
 
     def update_active(self):
+        """
+        Update function, runs if state is ACTIVE.
+
+        Resets node and activates next nodes.
+        """
         self.set_active(0)
         self.state = INACTIVE
 
     def update_waiting(self):
+        """
+        Update function, runs if state is WAITING.
+
+        If both inputs have value then function evaluates operation.
+        """
         if self.get_value(0) is not None and self.get_value(1) is not None:
             if self.name == "greater":
                 self.output_values[0] = self.get_value(0) > self.get_value(1)
@@ -52,15 +91,44 @@ class NodeLogicB(base.Node):
 
 
 class NodeIn(base.Node):
+    """
+    Class for node 'in' or 'contains'. Node checks whether
+    value is in list or in structure variable.
+    """
+
     def __init__(self, data):
+        """
+        Class constructor. Creates variables:
+            variant - mode of node:
+                all - node waits all signals from inputs;
+                any - node waits first signal from inputs.
+
+        :param dict data: node information
+        """
         super().__init__(data)
         self.variant = all if self.desc_value != "any" else any
 
     def update_active(self):
+        """
+        Update function, runs if state is ACTIVE.
+
+        Resets node and activates next nodes.
+        """
         self.set_active(0)
         self.state = INACTIVE
 
     def update_waiting(self):
+        """
+        Update function, runs if state is WAITING.
+
+        If node has no input nodes then function compares
+        description of node (desc_value) to exist structure variables.
+        If description has name of structure variables then function
+        checks whether value is in list of values (or keys for dict).
+
+        If node has input then based on mode (all or any) function
+        checks whether value matches with something in list of input values.
+        """
         if not self.inputs[0]:
             self.output_values[0] = False
             if self.desc_value in self.struct_variables:
@@ -83,16 +151,42 @@ class NodeIn(base.Node):
             self.state = ACTIVE
 
     def set_state(self, state, input_index, **kwargs):
+        """
+        Change state function, runs when other nodes are trying to activate current node.
+
+        If activation received from 'value' input (not from multiple input) then
+        function change node state.
+        """
         if state == WAITING and self.get_actual_input(input_index) == 1:
             self.state = state
 
 
 class NodeBool(base.Node):
+    """
+    Class for node 'bool'. Node converts any value to boolean value.
+    """
+
     def update_active(self):
+        """
+        Update function, runs if state is ACTIVE.
+
+        Resets node and activates next nodes.
+        """
         self.set_active(0)
         self.state = INACTIVE
 
     def update_waiting(self):
+        """
+        Update function, runs if state is WAITING.
+
+        If value is None then output value will be False.
+
+        Else value converts to boolean with rules similar to next rules:
+            1, 0.54, 504, True -> True
+            0, None, False - False
+            '' -> False (Empty string or char)
+            'some text' -> True
+        """
         if self.get_value(0) is None:
             self.output_values[0] = False
         else:
