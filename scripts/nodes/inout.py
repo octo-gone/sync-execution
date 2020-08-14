fa738a479bebbd619179419d45540f2be845e96c
octo-gone <nklimin007@gmail.com>
2020-08-14 11:23:04 +0300

    docs: added documentation for nodes in group 'inout' (input/output)

--- scripts/nodes/inout.py
+++ scripts/nodes/inout.py
@@ -5,7 +5,20 @@ import re
 
 
 class NodePrintCtrl(base.Node):
+    """
+    Class for node 'print with ctrl'. Print (output) node with subsequent activation.
+    """
+
     def update_waiting(self):
+        """
+        Update function, runs if state is WAITING.
+
+        Function takes value from description (desc_value). If this value
+        is a keyword ($vars and utils.program_values) then it generates
+        corresponding output. Otherwise function takes value from input.
+
+        After preparations function prints value and sets state to ACTIVE.
+        """
         value = self.desc_value
         if value == "$vars":
             variables = []
@@ -22,12 +35,30 @@ class NodePrintCtrl(base.Node):
         self.state = ACTIVE
 
     def update_active(self):
+        """
+        Update function, runs if state is ACTIVE.
+
+        Resets node and activates next nodes.
+        """
         self.set_active(0)
         self.state = INACTIVE
 
 
 class NodePrint(base.Node):
+    """
+    Class for node 'print'. Print (output) node.
+    """
+
     def update_waiting(self):
+        """
+        Update function, runs if state is WAITING.
+
+        Function takes value from description (desc_value). If this value
+        is a keyword ($vars and utils.program_values) then it generates
+        corresponding output. Otherwise function takes value from input.
+
+        After preparations function prints value and sets state to ACTIVE.
+        """
         value = self.desc_value
         if value == "$vars":
             variables = []
@@ -44,25 +75,43 @@ class NodePrint(base.Node):
         self.state = INACTIVE
 
     def update_active(self):
+        """
+        Update function, runs if state is ACTIVE.
+
+        Resets node.
+        """
         self.state = INACTIVE
 
 
 class NodeInput(base.Node):
+    """
+    Class for node 'input'. Input node with settable prompt for user.
+    It is possible to show value from program inside of prompt by using format string.
+    """
+
     def update_waiting(self):
+        """
+        Update function, runs if state is WAITING.
 
+        Prompt is value from node description (desc_value).
+        Function tries to find construction of type {<var name>$<type>} in prompt.
+
+        If it was found then function tries to generate string
+        version of value from variable.
+
+        If description wasn't changed then function takes '>>> ' (default) as prompt otherwise desc_value.
+
+        After taking value from input, function saves it to output and changes state to ACTIVE.
+        """
         pattern = r"{(?P<var>[^\{\}]+?)(?:\$(?P<type>[^\{\}]+?))?}"
-
         for match in re.finditer(pattern, self.desc_value):
-
             var_name = f"{self.scope}$" + match["var"]
             var_type = match["type"]
-
             if var_type is not None:
                 if var_type in utils.types_default:
                     var_type = utils.types_default[var_type].__class__
                 else:
                     var_type = utils.coercion
-
             if var_name in self.variables:
                 var_value = self.variables[var_name]
                 if var_type is not None:
@@ -70,7 +119,6 @@ class NodeInput(base.Node):
                 self.desc_value = list(self.desc_value)
                 self.desc_value[match.span()[0]:match.span()[1]] = list(str(var_value))
                 self.desc_value = "".join(self.desc_value)
-
         prompt = ">>> "
         if self.desc_value:
             if self.desc_value.endswith(" "):
@@ -82,6 +130,11 @@ class NodeInput(base.Node):
         self.state = ACTIVE
 
     def update_active(self):
+        """
+        Update function, runs if state is ACTIVE.
+
+        Resets node and activates next nodes.
+        """
         self.set_active(0)
         self.state = INACTIVE
 
