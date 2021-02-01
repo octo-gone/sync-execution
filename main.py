d9b8e3523e4c4c196d66398afd2061d159d78c8e
octo-gone <nklimin007@gmail.com>
2021-02-01 12:42:27 +0300

    feature: better choosing file added

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from scripts import parser, run
+import os
+import sys
+from scripts.config import config
 
-file = "script.drawio"
+if os.name == 'nt' and config.get("Console", "use_fileopen_dialog") == 'yes':
+    from tkinter.filedialog import askopenfilename
+    from tkinter import Tk
+    window = Tk()
+    window.withdraw()
+    # window.iconbitmap('icon.ico')
+    file = askopenfilename()
+else:
+    if len(sys.argv) > 1:
+        file = sys.argv[-1]
+    else:
+        file = input("Enter file name: ")
+
 run.run(*parser.parse(file))
 input("Press ENTER to exit...")
