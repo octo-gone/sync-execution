360c7bac615470db404d9c304982c81d3e3c96ed
octo-gone <naklimin@miem.hse.ru>
2021-02-10 15:53:04 +0300

    feature: file taking priority changed

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import os
 import sys
 from scripts.config import config
 
-if os.name == 'nt' and config.get("Console", "use_fileopen_dialog") == 'yes':
-    from tkinter.filedialog import askopenfilename
-    from tkinter import Tk
-    window = Tk()
-    window.withdraw()
-    # window.iconbitmap('icon.ico')
-    file = askopenfilename()
+if len(sys.argv) > 1:
+    file = sys.argv[-1]
 else:
-    if len(sys.argv) > 1:
-        file = sys.argv[-1]
+    if os.name == 'nt' and config.get("Console", "use_fileopen_dialog") == 'yes':
+        from tkinter.filedialog import askopenfilename
+        from tkinter import Tk
+        window = Tk()
+        window.withdraw()
+        # window.iconbitmap('icon.ico')
+        file = askopenfilename()
     else:
         file = input("Enter file name: ")
 
